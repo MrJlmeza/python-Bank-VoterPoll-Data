c4dae5e171a258286f8ed0b00f43bed6b1523718
MrJlmeza <mrjlmeza@gmail.com>
2021-04-06 06:28:29 -0400

    hw

---
+++ PyBank/main.py
@@ -0,0 +1,61 @@
+#-------import modules
+import os
+import csv
+
+#--------------set path for file
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+#----------create lists to store data
+Profits_Losses = []
+Monthly_changes = []
+Dates = []
+Greatest_Increase_Date = ""
+Greatest_Decrease_Date = ""
+
+
+#-----------read csv files using csv module
+with open(csvpath) as csvfile:
+    #-----------CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+    #set and fix header row prior to loop
+    csv_header = next(csvreader)
+    [print(f"CSV Header: {csv_header}")]
+    
+    #-----Loop through csv
+    for row in csvreader:
+        #begin obtaining values for lists and variables
+        Dates.append(row[0])
+        Profits_Losses.append(float(row[1]))
+
+    print("Financial Analysis")
+    print("----------------------------------")
+    print("Total Months:", len(Dates))
+    print("Net Total: $", sum(Profits_Losses)) 
+
+        
+        
+    for i in range(1, len(Profits_Losses)):
+        
+        #obtain profit changes between months, store values in list
+        Monthly_changes.append(Profits_Losses[i] - Profits_Losses[i-1])
+        
+        #Use Values in montly changes list to calculate average change
+        Average_Change = sum(Monthly_changes) / len(Monthly_changes)
+
+        #Use Values in in monthly changes list to determine greatest gain and loss and corresponding dates
+        Greatest_Profit_Increase = max(Monthly_changes)
+        Greatest_Increase_Date = str(Dates[Monthly_changes.index(max(Monthly_changes))])
+        Greatest_Profit_Decrease = min(Monthly_changes)
+        Greatest_Decrease_Date = str(Dates[Monthly_changes.index(min(Monthly_changes))])
+    #------------------Create print statements
+    print("Average Change: $", round(Average_Change))
+    print("Greatest Increase in Profits: ", Greatest_Increase_Date, "$", Greatest_Profit_Increase,")")
+    print("Greatest Decrease in Profits: ", Greatest_Decrease_Date, "($", Greatest_Profit_Decrease,")")
+
+    
+
+        
+        
+        
---
+++ PyPoll/main.py
@@ -0,0 +1,85 @@
+  
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+
+
+Votes = []
+Candidate_Votes = {}
+Candidates = []
+Winning_Votes = 0
+Winning_Candidates = ""
+
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    
+    print(csvreader)
+    
+    csv_header = next(csvreader)
+    [print(f"CSV Header: {csv_header}")]
+    
+    for row in csvreader:
+        Votes.append(row[0])
+
+        #candidates
+        Candidate = row[2]
+        #set list of non repeating candidates and their corresponding vote count
+        if Candidate not in Candidates:
+            Candidates.append(row[2])
+            Candidate_Votes[Candidate] = 0
+        
+        Candidate_Votes[Candidate] +=1
+
+   
+
+output_file = 'Analysis/PyPoll.txt'
+with open(output_file, "w", newline="") as datafile:
+
+    print("Election Results")
+    print("-------------------------------")
+    print("Total Votes:", len(Votes))
+    print("--------------------------------")
+
+
+    datafile.write("Election Results\n")
+    datafile.write("-------------------------------\n")
+    datafile.write(F"Total Votes: , len(Votes)\n")
+    datafile.write("--------------------------------\n")
+
+    
+    
+    for Candidate in Candidates:
+        Percentage = (round(float(Candidate_Votes[Candidate]) / len(Votes), 2)*100)
+
+        print(f"{Candidate}: {Percentage} ({Candidate_Votes[Candidate]})")
+
+        if Winning_Votes < Candidate_Votes[Candidate]:
+            Winning_Votes = Candidate_Votes[Candidate]
+            Winner = Candidate
+
+    print("----------------------------------------")
+    print("Winner: ", str(Winner))
+    print("----------------------------------------")
+
+    datafile.write("----------------------------------------\n")
+    datafile.write(F"Winner: , str(Winner)\n")
+    datafile.write("----------------------------------------")
+
+
+datafile.close()
+
+
+
+
+
+
+
+
+
+
+        
+    
